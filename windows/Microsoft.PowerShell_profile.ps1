5c25592d67ad6f27e961315d0a83b9d8c392f7c2
saratrajput <pattarsuraj@gmail.com>
2022-08-15 21:58:34 +0900

    :zap: Update powershell profile.

--- windows/Microsoft.PowerShell_profile.ps1
+++ windows/Microsoft.PowerShell_profile.ps1
@@ -1,17 +1,22 @@
 set-location d:
 # DGX
-function ssh-zero-dgx
+function zero-dgx
 {
 	ssh crdw@10.243.246.75
 }
-# DGX Direct
-function ssh-dgx
-{
-	ssh crdw@10.21.0.67
-}
 # Alienware
-function ssh-zero-alien
+function zero-alien
 {
-	ssh sp@10.243.87.248
+	ssh sp@10.243.69.244
+}
+# CR10
+function cr10
+{
+	ssh pi@192.168.0.110
+}
+# Prusa
+function prusa
+{
+	ssh pi@192.168.0.97
 }
 Clear-Host
